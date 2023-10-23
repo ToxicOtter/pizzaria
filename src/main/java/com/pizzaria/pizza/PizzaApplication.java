a27b430209ead92ef6c67294badfc7a345885558
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-22 21:25:52 -0300

    Create project

---
+++ src/main/java/com/pizzaria/pizza/PizzaApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaria.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaApplication.class, args);
+	}
+
+}
