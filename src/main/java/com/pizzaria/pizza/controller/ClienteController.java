2719a94b082ac0b4810d4d6f30329a5c86201434
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-23 16:03:28 -0300

    Create endpoint to receive data from added drinks and pizza

--- src/main/java/com/pizzaria/pizza/controller/ClienteController.java
+++ src/main/java/com/pizzaria/pizza/controller/ClienteController.java
@@ -3,9 +3,15 @@ package com.pizzaria.pizza.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.pizzaria.pizza.model.Cliente;
+import com.pizzaria.pizza.model.Produto;
 import com.pizzaria.pizza.model.bd.ClienteRepository;
 import com.pizzaria.pizza.model.bd.ProdutoRepository;
 
@@ -16,12 +22,27 @@ public class ClienteController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @GetMapping("/testeLista")
+    private List<List<Integer>> itens = new ArrayList<>();
+
+    @GetMapping("/home")
     public ModelAndView listarClientes() {
         ModelAndView mAv = new ModelAndView("cliente");
         mAv.addObject("listaDeClientes", clienteRepository.findAll());
         mAv.addObject("listaDePizzas", produtoRepository.findByTipo("Pizza"));
+        mAv.addObject("listaDeBebidas", produtoRepository.findByTipo("Bebida"));
         mAv.addObject("cliente", new Cliente());
+        mAv.addObject("produto", new Produto());
         return mAv;
+    }
+
+    @PostMapping("/salvarItem")
+    public String receberPizza(@RequestParam("id") int pizzaId, @RequestParam("qtd") int pizzaQtd) {
+        List<Integer> pizza = new ArrayList<Integer>();
+        pizza.add(pizzaId);
+        pizza.add(pizzaQtd);
+        System.out.println(pizza);
+        itens.add(pizza);
+        System.out.println(itens);
+        return "redirect:/home";
     }
 }
