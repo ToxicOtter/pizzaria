d67d1dd5ce4a917282b2a2c788afc944e7b15bbc
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-26 11:41:41 -0300

    Change sql insert values to get around the problem with utf-8

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,11 @@
 insert into cliente(email, cpf, nome, endereco_entrega, telefone, senha) values ('luan.lima.lopes16@gmail.com', 000, 'Luan', 'avenida', 991,'aaa');
 
-insert into produto(nome, descricao, valor, tipo) values ('Pizza mussarela','8 pedaços',49.9, 'Pizza');
-insert into produto(nome, descricao, valor, tipo) values ('Pizza peperoni','8 pedaços',54.8, 'Pizza');
-insert into produto(nome, descricao, valor, tipo) values ('Pizza portuguesa','8 pedaços',55.9, 'Pizza');
-insert into produto(nome, descricao, valor, tipo) values ('Pizza brócolis','8 pedaços',40.9, 'Pizza');
+insert into produto(nome, descricao, valor, tipo) values ('Pizza mussarela','Queijo e oregano',49.9, 'Pizza');
+insert into produto(nome, descricao, valor, tipo) values ('Pizza peperoni','Queijo, oregano e pepperoni',54.8, 'Pizza');
+insert into produto(nome, descricao, valor, tipo) values ('Pizza portuguesa','Queijo, presunto, ovo de codorna, azeitona preta, cebola, oregano e pimentao verde',55.9, 'Pizza');
+insert into produto(nome, descricao, valor, tipo) values ('Pizza brocolis','Queijo e brocolis',40.9, 'Pizza');
 
-insert into produto(nome, descricao, valor, tipo) values ('Coca Cola','200ml - lata',8.9, 'Bebida');
-insert into produto(nome, descricao, valor, tipo) values ('Coca Cola - Zero','200ml - lata',8.9, 'Bebida');
-insert into produto(nome, descricao, valor, tipo) values ('Coca Cola','2L',14.9, 'Bebida');
-insert into produto(nome, descricao, valor, tipo) values ('Guaraná Antartica','200ml - lata',8.9, 'Bebida');
-insert into produto(nome, descricao, valor, tipo) values ('Guaraná Antartica','2L',13.9, 'Bebida');
+insert into produto(nome, descricao, valor, tipo) values ('Coca Cola','',5.0, 'Bebida');
+insert into produto(nome, descricao, valor, tipo) values ('Coca Cola - Zero','',4.0, 'Bebida');
+insert into produto(nome, descricao, valor, tipo) values ('Guarana Antartica','',5.0, 'Bebida');
+insert into produto(nome, descricao, valor, tipo) values ('Guarana Antartica - Zero','',4.0, 'Bebida');
