d16cfc9bd4a87cc4c427048fd40ef2f396fb8d49
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-26 10:55:38 -0300

    Add address change on order and fix some issues

--- src/main/resources/static/finalizar.js
+++ src/main/resources/static/finalizar.js
@@ -1,16 +1,16 @@
 let divCarrinho = document.getElementById("carrinho");
 let divInfoCliente = document.getElementById("info-cliente");
 let divFormaPagamento = document.getElementById("forma-pagamento");
+let divEnderecoAlternativo = document.getElementById("endereco-alternativo");
 
 let pagamento = document.getElementById("pagamento");
 let pagamentoSelecionado = pagamento.options[pagamento.selectedIndex].text;
-let endereco = document.getElementById("enderecoCliente").value;
+let endereco = document.getElementById("enderecoCliente").textContent;
+let enderecoNovo = document.getElementById("novoEndereco");
 let valor = document.getElementById("total").textContent;
 
-function checar(){
-    console.log(pagamento.options[pagamento.selectedIndex].text);
-    console.log(endereco);
-}
+let enderecoCadastro = true;
+let enderecoNovoValor;
 
 function carrinhoVisivel(){
     divCarrinho.style.visibility = "visible";
@@ -30,6 +30,17 @@ function formaPagamentoVisivel(){
     divFormaPagamento.style.visibility = "visible";
 }
 
+function trocarEndereco(trocar){
+    console.log(typeof trocar);
+    if(trocar){
+        enderecoCadastro = trocar;
+        divEnderecoAlternativo.style.visibility = "hidden";
+    } else {
+        enderecoCadastro = trocar;
+        divEnderecoAlternativo.style.visibility = "visible";
+    }
+}
+
 function deletar(){
     if(window.confirm("Realmente deseja cancelar o pedido?") == true){
         window.location.replace("/deletarPedido");
@@ -37,11 +48,19 @@ function deletar(){
 }
 
 function finalizar(){
-    window.location.replace("/encerrar?pagamento="+pagamentoSelecionado+"&valor="+valor+"&endereco="+endereco);
-    console.log(pagamento);
-    console.log(valor);
+    if(enderecoCadastro){
+        window.location.replace("/encerrar?pagamento="+pagamentoSelecionado+"&valor="+valor+"&endereco="+endereco);
+    } else {
+        window.location.replace("/encerrar?pagamento="+pagamentoSelecionado+"&valor="+valor+"&endereco="+enderecoNovoValor);
+    }
 }
 
 function voltarParaItens(){
     window.location.replace("/selecionarItens");
 }
+
+const inputHandler = function(e){
+    enderecoNovoValor = e.target.value;
+}
+
+enderecoNovo.addEventListener('input', inputHandler);
--- src/main/resources/static/produtos.js
+++ src/main/resources/static/produtos.js
@@ -9,13 +9,11 @@ let valorTotalUnitario = valorUnitario;
 inputQtd.addEventListener("input", updateValueQtd);
 
 function updateValueQtd(e) {
-  console.log("qtd = " + e.target.value);
   qtd = parseInt(e.target.value);
   calcularValor();
 }
 
 function listQ(){
-    console.log("tamanho =" + this.value);
     tamanho = this.value;
     calcularValor();
 }
@@ -25,10 +23,8 @@ function calcularValor(){
     if(tamanho == "8"){
         valorTotalUnitario = (valorUnitario + 10) * qtd;
         valorUnitarioTotalEl.value = valorTotalUnitario;
-        console.log(valorTotalUnitario);
     } else {
         valorTotalUnitario = (valorUnitario + 5) * qtd;
         valorUnitarioTotalEl.value = valorTotalUnitario;
-        console.log(valorTotalUnitario);
     }
 }
